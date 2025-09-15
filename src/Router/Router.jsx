f03c5679aad0b5f17d21b33edda701afbf5c04e9
A K M Shafee Ullah <187232344+shafee-ullah@users.noreply.github.com>
2025-09-15 19:52:27 +0600

    dashboard created

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,20 +1,30 @@
 import { createBrowserRouter } from "react-router";
-import RootLayouts from '../Layouts/RootLayouts'
-import Home from "../Pages/Home/Home/Home"
-
+import RootLayouts from "../Layouts/RootLayouts";
+import Home from "../Pages/Home/Home/Home";
+import DashboardLayout from "../Pages/Dashboard/DashboardLayout";
+import GuestDashboard from "../Pages/Dashboard/GuestDashboard";
+import HostDashboard from "../Pages/Dashboard/HostDashboard";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        Component: RootLayouts,
+  {
+    path: "/",
+    Component: RootLayouts,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "dashboard",
+        Component: DashboardLayout,
         children: [
-            {
-                index: true,
-                Component: Home
-            }
-        ]
-    },
+          { index: true, Component: GuestDashboard },
+          { path: "guest", Component: GuestDashboard },
+          { path: "host", Component: HostDashboard },
+        ],
+      },
+    ],
+  },
 ]);
 
-
-export default router; 
+export default router;
