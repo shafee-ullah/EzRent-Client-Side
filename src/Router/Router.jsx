23b6eef4babbc0d0aaa31c2cb75b18a98138912b
Tanvir islam <tanvirislam4147@gmail.com>
2025-09-16 21:09:02 +0600

    card and card ditels page change style and layout

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,10 +1,8 @@
-import { createBrowserRouter } from "react-router";
-import RootLayouts from '../Layouts/RootLayouts'
-import Home from "../Pages/Home/Home/Home"
-import FeaturepropertiesDitels from "../Pages/Home/FeaturedProperties/FeaturepropertiesDitels";
 
+import { createBrowserRouter } from "react-router";
 import RootLayouts from "../Layouts/RootLayouts";
 import Home from "../Pages/Home/Home/Home";
+import FeaturepropertiesDitels from "../Pages/Home/FeaturedProperties/FeaturepropertiesDitels";
 import DashboardLayout from "../Pages/Dashboard/DashboardLayout";
 import GuestDashboard from "../Pages/Dashboard/GuestDashboard";
 import HostDashboard from "../Pages/Dashboard/HostDashboard";
@@ -18,21 +16,14 @@ const router = createBrowserRouter([
         index: true,
         Component: Home,
       },
-                  {
-              path: "/FeaturepropertiesDitels/:id",
-              Component:FeaturepropertiesDitels
-            },
+      {
+        path: "FeaturepropertiesDitels/:id",
+        Component: FeaturepropertiesDitels,
+      },
       {
         path: "dashboard",
         Component: DashboardLayout,
         children: [
-            {
-                index: true,
-                Component: Home
-            },
-
-        ]
-    },
           { index: true, Component: GuestDashboard },
           { path: "guest", Component: GuestDashboard },
           { path: "host", Component: HostDashboard },
