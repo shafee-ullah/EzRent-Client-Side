8b7ad291b900ca7159a924d7ca85f73689429f79
Shakib0976 <web.shakib09@gmail.com>
2025-09-26 01:43:17 +0600

    change navbar section

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -58,25 +58,25 @@ const Banner = () => {
 
     return (
         <div
-            className="relative min-h-screen mb-10 px-2 flex py-5 md:py-10 lg:py-20 items-center justify-center bg-cover bg-center"
+            className="relative min-h-[50vh] max-w-11/12 mx-auto my-5 rounded-2xl mb-10 px-2 flex py-5 md:py-10 lg:py-20 items-center justify-center bg-cover bg-center"
             style={{
-                backgroundImage: `url(${images[currentImage]})`,
+                backgroundImage: `url(https://i.ibb.co.com/DfYxJkQ1/logo-make-11-06-2023-9-1.jpg)`,
             }}
         >
             {/* Overlay */}
-            <div className="absolute inset-0 bg-slate-950/30 dark:bg-slate-950/50" />
+            <div className="absolute inset-0 rounded-2xl dark:bg-slate-950/50" />
 
             <div className="relative z-10 flex flex-col md:items-start items-center text-start text-white px-4">
                 <h1 className="text-3xl md:text-5xl font-bold">
                     Find Your Perfect <span className="text-green-600">Getaway</span>
                 </h1>
-                <p className="mt-4 mb-10  max-w-3xl text-sm md:text-lg">
+                <p className="mt-4 mb-5  max-w-3xl text-sm md:text-lg">
                     Discover unique accommodations around the world. From  cozy cabins to
                     luxury villas, create unforgettable memories with HopNow.
                 </p>
 
                 {/* Rotating Message Card with Fade Animation */}
-                <div className="mb-5 md:mb-10 w-full max-w-2xl">
+                <div className="mb-5  w-full max-w-2xl bg-white/20 border border-white/30 backdrop-blur-md shadow-lg rounded-xl text-start py-5">
                     <AnimatePresence mode="wait">
                         <motion.div
                             key={currentMsg}
@@ -84,7 +84,7 @@ const Banner = () => {
                             animate={{ opacity: 1, y: 0 }}
                             exit={{ opacity: 0, y: -20 }}
                             transition={{ duration: 0.6 }}
-                            className="bg-white/20 border border-white/30 backdrop-blur-md shadow-lg rounded-xl p-5"
+                            className=""
                         >
                             <p className="text-white text-center text-lg md:text-xl font-medium">
                                 {messages[currentMsg]}
@@ -95,13 +95,13 @@ const Banner = () => {
 
                 <div className="flex flex-col items-center">
                     {/* Search Box */}
-                    <div className="relative mt-8 w-full max-w-5xl rounded-2xl bg-gradient-to-r from-green-400 via-blue-500 to-purple-500 animate-border">
-                        <div className="bg-[var(--primary-color)] rounded-2xl shadow-lg p-6">
+                    <div className="relative mt-8 w-full max-w-5xl  rounded-2xl">
+                        <div className="bg-gray-100 dark:bg-gray-900 rounded-2xl shadow-lg p-6 ">
                             <form onSubmit={handleSubmitFormData}>
                                 <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
                                     {/* Destination */}
                                     <div className="flex flex-col">
-                                        <label className="text-gray-500 dark:text-gray-300 text-sm mb-1">Where to?</label>
+                                        <label className="text-gray-900 dark:text-gray-300 text-sm mb-1">Where to?</label>
                                         <div className="flex items-center border border-gray-300 rounded-lg px-3 py-2">
                                             <FaMapMarkerAlt className="text-gray-400 dark:text-gray-300 mr-2" />
                                             <input
@@ -117,7 +117,7 @@ const Banner = () => {
 
                                     {/* Check-in */}
                                     <div className="flex flex-col">
-                                        <label className="text-gray-500 dark:text-gray-300 text-sm mb-1">Check-in</label>
+                                        <label className="text-gray-900 dark:text-gray-300 text-sm mb-1">Check-in</label>
                                         <div className="flex items-center border border-gray-300 rounded-lg px-3 py-2">
                                             <HiOutlineCalendar className="text-gray-400 mr-2" />
                                             <input
@@ -132,7 +132,7 @@ const Banner = () => {
 
                                     {/* Check-out */}
                                     <div className="flex flex-col">
-                                        <label className="text-gray-500 dark:text-gray-300 text-sm mb-1">Check-out</label>
+                                        <label className="text-gray-900 dark:text-gray-300 text-sm mb-1">Check-out</label>
                                         <div className="flex items-center border border-gray-300 rounded-lg px-3 py-2">
                                             <HiOutlineCalendar className="text-gray-400 mr-2" />
                                             <input
@@ -147,7 +147,7 @@ const Banner = () => {
 
                                     {/* Guests */}
                                     <div className="flex flex-col">
-                                        <label className="text-gray-500 dark:text-gray-300 text-sm mb-1">Guests</label>
+                                        <label className="text-gray-900 dark:text-gray-300 text-sm mb-1">Guests</label>
                                         <div className="flex items-center border border-gray-300 rounded-lg px-3 py-2">
                                             <FaUser className="text-gray-400 mr-2" />
                                             <input
